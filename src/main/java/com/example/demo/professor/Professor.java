8aa5d7eeeadec30e5548f72c1a941c24d1f19dad
smaslou1 <123901540+smaslou1@users.noreply.github.com>
2023-02-23 01:09:14 +0100

    add Professor class

---
+++ src/main/java/com/example/demo/professor/Professor.java
@@ -0,0 +1,32 @@
+package com.example.demo.professor;
+
+
+public class Professor {
+    private String name;
+    private int professorID;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getProfessorID() {
+        return professorID;
+    }
+
+    public void setProfessorID(int professorID) {
+        this.professorID = professorID;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
